dd02a7c28bfcf33b6da97cc36daef96e6443847f
Christian von Wascinski <harryherold@googlemail.com>
2023-10-15 16:25:02 +0200

    feature: Add saving notes as todo.txt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-// use slint::{FilterModel, Model, SortModel};
-use std::rc::Rc;
+use std::fs::File;
+use std::{rc::Rc, path::PathBuf};
+use std::io::{Write, Error};
 
-use slint::Model;
+use slint::{Model, VecModel};
+
+use native_dialog::{FileDialog, MessageDialog, MessageType};
 
 slint::include_modules!();
+
+fn save_vec_model(vec_model: &VecModel<ReviewTodoItem>, path: PathBuf) -> Result<(), Error> {
+    let mut file = File::create(path)?;
+    for item in vec_model.iter() {
+        let task = todo_txt::Task {
+            subject: item.text.to_string(),
+            finished: item.isFixed,
+            ..Default::default()
+        };
+        write!(file, "{}\n", task.to_string())?;
+    }
+    Ok(())
+}
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
@@ -43,6 +59,26 @@ fn main() -> Result<(), slint::PlatformError> {
                     text: item.text,
                 });
             }
+        }
+    });
+    ui.on_review_todos_save_requested({
+        let todo_model = todo_model.clone();
+        move || {
+            let save_option = FileDialog::new()
+            .set_location("~")
+            .add_filter("Text File (*.txt)", &["txt"])
+            .show_save_single_file()
+            .unwrap();
+            if let Some(save_path) = save_option {
+                let result = save_vec_model(&todo_model, save_path);
+                if let Err(_) = result {
+                    let _r = MessageDialog::new()
+                        .set_type(MessageType::Error)
+                        .set_title("Abort")
+                        .set_text("Could save comments!")
+                        .show_alert();
+                }
+            };
         }
     });
 
