68c5f4631d6e6b040d7887f7445cf1ad4006e1a5
Christian von Wascinski <harryherold@googlemail.com>
2023-10-17 22:25:54 +0200

    feature: structure code.

---
+++ src/lib.rs
@@ -0,0 +1,137 @@
+use std::fs::OpenOptions;
+use std::fs::{read_to_string, File};
+use std::io::{Error, Write};
+use std::str::FromStr;
+use std::{path::PathBuf, rc::Rc};
+
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+
+use slint::{Model, SharedString, VecModel};
+
+slint::include_modules!();
+
+fn write_todos_to_file(vec_model: &VecModel<ReviewTodoItem>, path: &PathBuf, should_create_file: bool) -> Result<(), Error> {
+    let get_file = || {
+        if should_create_file {
+            return File::create(path);
+        } else {
+            return OpenOptions::new().write(true).open(path);
+        }
+    };
+    let mut file = get_file()?;
+    for item in vec_model.iter() {
+        let task = todo_txt::Task {
+            subject: item.text.to_string(),
+            finished: item.isFixed,
+            ..Default::default()
+        };
+        write!(file, "{}\n", task.to_string())?;
+    }
+    Ok(())
+}
+
+fn read_todos_from_file(todo_model: &VecModel<ReviewTodoItem>, path: &PathBuf) -> Result<(), Error> {
+    for line in read_to_string(path)?.lines() {
+        let task_result = todo_txt::Task::from_str(line);
+        if let Ok(task) = task_result {
+            todo_model.push(ReviewTodoItem {
+                isFixed: task.finished,
+                text: task.subject.into(),
+            });
+        } else {
+            ()
+        }
+    }
+    Ok(())
+}
+
+pub struct Review {
+    todo_model: Rc<VecModel<ReviewTodoItem>>,
+    todo_file: PathBuf,
+}
+impl Review {
+    pub fn new() -> Review {
+        Review {
+            todo_model: Rc::new(slint::VecModel::<ReviewTodoItem>::default()),
+            todo_file: PathBuf::new(),
+        }
+    }
+
+    pub fn todo_model(&self) -> Rc<VecModel<ReviewTodoItem>> {
+        self.todo_model.clone()
+    }
+
+    pub fn add_todo(&self, text: SharedString) {
+        self.todo_model.push(ReviewTodoItem { isFixed: false, text: text })
+    }
+
+    pub fn open_todos(&mut self) -> Option<SharedString> {
+        self.todo_file = PathBuf::new();
+        if self.todo_file.as_os_str().is_empty() {
+            self.todo_file = FileDialog::new()
+                .set_location("~")
+                .add_filter("Text File (*.txt)", &["txt"])
+                .show_open_single_file()
+                .unwrap()?;
+        }
+        let result = read_todos_from_file(&self.todo_model, &self.todo_file);
+        if result.is_err() {
+            let _ = MessageDialog::new()
+                .set_type(MessageType::Error)
+                .set_title("Error")
+                .set_text("Errors occured during reading file!")
+                .show_alert();
+            return None;
+        }
+        Some(SharedString::from(self.todo_file.to_str()?))
+    }
+
+    pub fn save_todos(&mut self) -> Option<SharedString> {
+        let should_create_file = self.todo_file.as_os_str().is_empty();
+        if should_create_file {
+            let path = FileDialog::new()
+                .set_location("~")
+                .add_filter("Text File (*.txt)", &["txt"])
+                .show_save_single_file()
+                .unwrap()?;
+            self.todo_file = path;
+        }
+        let result = write_todos_to_file(&self.todo_model, &self.todo_file, should_create_file);
+        if let Err(_) = result {
+            let _r = MessageDialog::new()
+                .set_type(MessageType::Error)
+                .set_title("Abort")
+                .set_text("Could save comments!")
+                .show_alert();
+        }
+        Some(SharedString::from(self.todo_file.to_str()?))
+    }
+
+    pub fn toogle_is_fixed(&self, todo_index: usize) {
+        let data = self.todo_model.row_data_tracked(todo_index);
+        if let Some(item) = data {
+            self.todo_model.set_row_data(
+                todo_index as usize,
+                ReviewTodoItem {
+                    isFixed: !item.isFixed,
+                    text: item.text,
+                },
+            );
+        }
+    }
+
+    pub fn set_todo_text(&self, todo_index: usize, text: SharedString) {
+        let data = self.todo_model.row_data_tracked(todo_index);
+        if let Some(item) = data {
+            if item.text != text {
+                self.todo_model.set_row_data(
+                    todo_index,
+                    ReviewTodoItem {
+                        isFixed: item.isFixed,
+                        text: text,
+                    },
+                );
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,44 @@
-use std::fs::{File, read_to_string};
-use std::str::FromStr;
-use std::{rc::Rc, path::PathBuf};
-use std::io::{Write, Error};
+use std::{cell::RefCell, rc::Rc};
 
-use slint::{Model, VecModel};
-
-use native_dialog::{FileDialog, MessageDialog, MessageType};
-
-slint::include_modules!();
-
-fn save_vec_model(vec_model: &VecModel<ReviewTodoItem>, path: PathBuf) -> Result<(), Error> {
-    let mut file = File::create(path)?;
-    for item in vec_model.iter() {
-        let task = todo_txt::Task {
-            subject: item.text.to_string(),
-            finished: item.isFixed,
-            ..Default::default()
-        };
-        write!(file, "{}\n", task.to_string())?;
-    }
-    Ok(())
-}
-
-// TODO create own error enum
-fn open_vec_model(vec_model: &VecModel<ReviewTodoItem>, path: PathBuf) -> Result<(), Error> {
-    for line in read_to_string(path)?.lines() {
-        let task_result = todo_txt::Task::from_str(line);
-        if let Ok(task) = task_result {
-            vec_model.push(ReviewTodoItem { 
-                isFixed: task.finished, text: task.subject.into()
-            });
-        }
-    }
-    Ok(())
-}
+use review_todo::{AppWindow, Review};
+use slint::ComponentHandle;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-    let ui_weak = ui.as_weak();
-
-    let todo_model = Rc::new(slint::VecModel::<ReviewTodoItem>::default());
+    let review = Rc::new(RefCell::new(Review::new()));
 
     ui.on_review_todo_added({
-        let todo_model = todo_model.clone();
-        move | text |{
-            todo_model.push(ReviewTodoItem{ isFixed: false, text: text });
-        }
+        let review = review.clone();
+        move |text| review.borrow().add_todo(text)
     });
     ui.on_review_todo_text_changed({
-        let todo_model = todo_model.clone();
-        move | index, text | {
-            let data = todo_model.row_data_tracked(index as usize);
-            if let Some(item) = data {
-                if item.text != text {
-                    todo_model.set_row_data(index as usize, ReviewTodoItem{
-                        isFixed: item.isFixed,
-                        text: text,
-                    });
-                }
-            }
-        }
+        let review = review.clone();
+        move |todo_index, text| review.borrow().set_todo_text(todo_index as usize, text)
     });
     ui.on_review_todo_is_fixed_toggled({
-        let todo_model = todo_model.clone();
-        move | index | {
-            let data = todo_model.row_data_tracked(index as usize);
-            if let Some(item) = data {
-                todo_model.set_row_data(index as usize, ReviewTodoItem{
-                    isFixed: !item.isFixed,
-                    text: item.text,
-                });
-            }
-        }
+        let review = review.clone();
+        move |todo_index| review.borrow().toogle_is_fixed(todo_index as usize)
     });
     ui.on_review_todos_save_requested({
-        let todo_model = todo_model.clone();
+        let review = review.clone();
+        let ui_weak = ui.as_weak();
         move || {
-            let save_option = FileDialog::new()
-            .set_location("~")
-            .add_filter("Text File (*.txt)", &["txt"])
-            .show_save_single_file()
-            .unwrap();
-            // TODO use ui.get_current_file()
-            if let Some(save_path) = save_option {
-                let result = save_vec_model(&todo_model, save_path);
-                if let Err(_) = result {
-                    let _r = MessageDialog::new()
-                        .set_type(MessageType::Error)
-                        .set_title("Abort")
-                        .set_text("Could save comments!")
-                        .show_alert();
-                }
-            };
+            if let Some(path) = review.borrow_mut().save_todos() {
+                let ui = ui_weak.unwrap();
+                ui.set_current_file(path);
+            }
         }
     });
     ui.on_review_todos_open_requested({
-        let todo_model = todo_model.clone();
+        let review = review.clone();
+        let ui_weak = ui.as_weak();
         move || {
-            let open_option = FileDialog::new()
-                .set_location("~")
-                .add_filter("Text File (*.txt)", &["txt"])
-                .show_open_single_file()
-                .unwrap();
-            if let Some(open_path) = open_option {
-                let ui = ui_weak.unwrap();
-                ui.set_current_file(open_path.to_str().unwrap().into());
-                open_vec_model(&todo_model, open_path);
+            let ui = ui_weak.unwrap();
+            if let Some(path) = review.borrow_mut().open_todos() {
+                ui.set_current_file(path);
             }
         }
     });
-    
-    ui.set_review_todo_item_model(todo_model.into());
-
+    ui.set_review_todo_item_model(review.borrow().todo_model().into());
     ui.run()
 }
