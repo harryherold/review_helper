7b655b63265a13ce06c21392b94aabb2ca51402d
Christian von Wascinski <harryherold@googlemail.com>
2024-12-13 13:56:29 +0100

    feature: Add file stats.

--- src/git_utils.rs
+++ src/git_utils.rs
@@ -1,4 +1,10 @@
-use std::{path::PathBuf, process::Command};
+use std::{collections::HashMap, path::PathBuf, process::Command};
+
+#[derive(Debug)]
+pub struct FileStat {
+    pub added_lines: u32,
+    pub removed_lines: u32,
+}
 
 pub fn is_git_repo(path: &PathBuf) -> bool {
     let git_folder = path.join(PathBuf::from(".git"));
@@ -12,8 +18,8 @@ pub fn repo_contains_commit(path: &PathBuf, commit: &str) -> anyhow::Result<bool
     Ok(msg.contains("commit"))
 }
 
-pub fn diff_git_repo(repo_path: &PathBuf, start_commit: &str, end_commit: &str) -> anyhow::Result<String> {
-    let mut args = vec!["diff", "--name-only"];
+pub fn diff_git_repo(repo_path: &PathBuf, start_commit: &str, end_commit: &str) -> anyhow::Result<HashMap<String, FileStat>> {
+    let mut args = vec!["diff", "--numstat"];
 
     if false == start_commit.is_empty() {
         args.push(start_commit);
@@ -23,8 +29,33 @@ pub fn diff_git_repo(repo_path: &PathBuf, start_commit: &str, end_commit: &str) 
     }
 
     let output = Command::new("git").current_dir(repo_path).args(args).output()?;
+    let string_output = String::from_utf8(output.stdout.trim_ascii().to_vec())?;
 
-    String::from_utf8(output.stdout).map_err(|e| anyhow::Error::from(e))
+    let mut files_stats: HashMap<String, FileStat> = HashMap::new();
+    let parse_line_number = |number_str: &str| -> anyhow::Result<u32> {
+        if number_str.contains("-") {
+            Ok(0)
+        } else {
+            number_str.parse::<u32>().map_err(|e| anyhow::format_err!(e.to_string()))
+        }
+    };
+
+    for line in string_output.lines().collect::<Vec<&str>>() {
+        if line.is_empty() {
+            continue;
+        }
+        let infos = line.split_whitespace().collect::<Vec<&str>>();
+
+        assert_eq!(infos.len(), 3);
+
+        let key = infos[2].to_string();
+        let value = FileStat {
+            added_lines: parse_line_number(infos[0])?,
+            removed_lines: parse_line_number(infos[1])?,
+        };
+        files_stats.insert(key, value);
+    }
+    Ok(files_stats)
 }
 
 pub fn diff_file(repo_path: &PathBuf, start_commit: &str, end_commit: &str, file: &str) -> anyhow::Result<()> {
@@ -48,4 +79,66 @@ pub fn first_commit(repo_path: &PathBuf) -> anyhow::Result<String> {
     let output = Command::new("git").current_dir(repo_path).args(args).output()?;
 
     String::from_utf8(output.stdout.trim_ascii().to_vec()).map_err(|e| anyhow::Error::from(e))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, path::PathBuf, process::Command};
+
+    #[derive(Debug)]
+    struct FileStat {
+        added_lines: u32,
+        removed_lines: u32,
+    }
+    enum ChangeType {
+        New,
+        Gone,
+        Modify,
+    }
+    impl ChangeType {
+        fn as_str(&self) -> &str {
+            match *self {
+                ChangeType::New => "A",
+                ChangeType::Gone => "D",
+                ChangeType::Modify => "M",
+            }
+        }
+    }
+
+    #[test]
+    fn test_query_diff_stats() {
+        let repo_path = PathBuf::from("/home/harry/workspace/review-todo");
+        let start_commit = "c13426875795b97d79c03c1dbf56dc2c87164b34";
+        let end_commit = "98bad3587d5810b300959bdb6c79b811c8b1c2cd";
+        let args = vec!["diff", start_commit, end_commit, "--numstat"];
+        let output = Command::new("git")
+            .current_dir(repo_path)
+            .args(args)
+            .output()
+            .expect("git diff numstats not working!");
+        let string_output = String::from_utf8(output.stdout.trim_ascii().to_vec()).expect("String conversion invalid!");
+        let mut files_stats: HashMap<String, FileStat> = HashMap::new();
+
+        let parse_line_number = |number_str: &str| -> anyhow::Result<u32> {
+            if number_str.contains("-") {
+                Ok(0)
+            } else {
+                number_str.parse::<u32>().map_err(|e| anyhow::format_err!(e.to_string()))
+            }
+        };
+
+        for line in string_output.lines().collect::<Vec<&str>>() {
+            let infos = line.split_whitespace().collect::<Vec<&str>>();
+            assert_eq!(infos.len(), 3);
+
+            let key = infos[2].to_string();
+            let value = FileStat {
+                added_lines: parse_line_number(infos[0]).expect("parse error"),
+                removed_lines: parse_line_number(infos[1]).expect("parse error"),
+            };
+            println!("{} => {:?}", &key, &value);
+            files_stats.insert(key, value);
+        }
+        // println!("lines {}", lines.len());
+    }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::{path::PathBuf, rc::Rc};
 
 use native_dialog::FileDialog;
@@ -23,7 +23,6 @@ impl Diff {
             start_commit: String::new(),
             end_commit: String::new(),
             file_diff_model: Rc::new(slint::VecModel::<ui::DiffFileItem>::default()),
-            // files: HashMap::new(),
         }
     }
 }
@@ -50,6 +49,8 @@ impl Repository {
                 repo.current_diff.file_diff_model.push(ui::DiffFileItem {
                     text: diff_file.file_name.to_owned().into(),
                     is_reviewed: diff_file.is_reviewed,
+                    added_lines: -1,
+                    removed_lines: -1,
                 })
             }
         }
@@ -90,22 +91,59 @@ impl Repository {
         self.current_diff.start_commit = start_commit.to_string();
         self.current_diff.end_commit = end_commit.to_string();
 
-        let output_text = git_utils::diff_git_repo(&self.path, &start_commit, &end_commit)?;
+        let files_stats = git_utils::diff_git_repo(&self.path, &start_commit, &end_commit)?;
 
-        let old_files: HashSet<String> = self.current_diff.file_diff_model.iter().map(|item| item.text.to_string()).collect();
-        let diff_files: HashSet<String> = output_text.split('\n').filter(|file| false == file.is_empty()).map(|f| f.to_string()).collect();
+        let mut old_files: HashSet<String> = HashSet::new();
+        let mut file_index_map: HashMap<String, usize> = HashMap::new();
+
+        self.current_diff
+            .file_diff_model
+            .iter()
+            .map(|item| item.text.to_string())
+            .enumerate()
+            .for_each(|(index, file)| {
+                file_index_map.insert(file.to_owned(), index);
+                old_files.insert(file);
+            });
+
+        let diff_files: HashSet<String> = files_stats.keys().cloned().collect();
+
+        let update_item = |mut item: ui::DiffFileItem, row: usize| {
+            let file_stat = files_stats.get(item.text.as_str()).unwrap();
+
+            if item.added_lines != file_stat.added_lines as i32 || item.removed_lines != file_stat.removed_lines as i32 {
+                item.added_lines = file_stat.added_lines as i32;
+                item.removed_lines = file_stat.removed_lines as i32;
+                self.current_diff.file_diff_model.set_row_data(row, item);
+            }
+        };
+        let add_item = |file: &String| {
+            let file_stat = files_stats.get(file).unwrap();
+            self.current_diff.file_diff_model.push(ui::DiffFileItem {
+                text: file.into(),
+                is_reviewed: false,
+                added_lines: file_stat.added_lines as i32,
+                removed_lines: file_stat.removed_lines as i32,
+            });
+        };
 
         if diff_files == old_files {
+            for (row, item) in self.current_diff.file_diff_model.iter().enumerate() {
+                update_item(item, row);
+            }
             return Ok(());
         } else if diff_files.is_disjoint(&old_files) {
             self.current_diff.file_diff_model.clear();
-            diff_files.into_iter().for_each(|file| {
-                self.current_diff.file_diff_model.push(ui::DiffFileItem {
-                    text: file.into(),
-                    is_reviewed: false,
-                })
-            });
+            diff_files.iter().for_each(add_item);
         } else {
+            let modified_files = old_files.intersection(&diff_files).collect::<HashSet<&String>>();
+            for modified_file in modified_files {
+                let index = file_index_map.get(modified_file).expect("Modified files should not be deleted!");
+                if let Some(item) = self.current_diff.file_diff_model.row_data(*index) {
+                    update_item(item, *index);
+                }
+            }
+
             let deleted_files: HashSet<&String> = old_files.difference(&diff_files).collect();
             for deleted_file in deleted_files {
                 let delete_item = self.current_diff.file_diff_model.iter().enumerate().find(|(_, item)| item.text == deleted_file);
@@ -114,12 +152,7 @@ impl Repository {
                 }
             }
             let new_files: HashSet<&String> = diff_files.difference(&old_files).collect();
-            for new_file in new_files {
-                self.current_diff.file_diff_model.push(ui::DiffFileItem {
-                    text: new_file.into(),
-                    is_reviewed: false,
-                })
-            }
+            new_files.into_iter().for_each(add_item);
         }
         Ok(())
     }
