5e1829066bd8f8447316ff6f8bb36aa8b4d3ac65
Christian von Wascinski <harryherold@googlemail.com>
2025-08-22 11:19:16 +0200

    feature(application_icon): Add application icon for windows.

--- build.rs
+++ build.rs
@@ -1,4 +1,15 @@
-fn main() {
+use std::{env, io};
+use winresource::WindowsResource;
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("assets/icons/review_helper.ico")
+            .compile()?;
+    }
+
     let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
     slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
+    Ok(())
 }
