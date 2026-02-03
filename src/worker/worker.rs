41fdd74351b654b4aa9557ed1f82ea79cb30798c
Christian von Wascinski <harryherold@googlemail.com>
2026-02-03 09:34:48 +0100

    fix(CommitsModel): Load commit when repository is created.

--- src/worker/worker.rs
+++ src/worker/worker.rs
@@ -274,7 +274,11 @@ impl WorkerImpl {
                     editor_args,
                     color_scheme,
                 } => self.save_settings(diff_tool, editor, editor_args, color_scheme),
-                WorkerMessage::NewRepository(path) => self.new_repository(path),
+                WorkerMessage::NewRepository(path) => {
+                    if let Some(repository_id) = self.new_repository(path) {
+                        self.load_commits(&repository_id);
+                    }
+                }
                 WorkerMessage::ChangeRepository { id, base_branch } => self.change_repository(id, base_branch),
                 WorkerMessage::LoadRepository { id } => {
                     self.load_commits(&id);
@@ -322,11 +326,11 @@ impl WorkerImpl {
             self.ui_updater.report_error(ui::SlintResult::StoreFailed, &e.to_string());
         }
     }
-    fn new_repository(&mut self, path: PathBuf) {
+    fn new_repository(&mut self, path: PathBuf) -> Option<RepositoryId> {
         if self.repositories.contains_repository_path(&path) {
             self.ui_updater
                 .report_error(ui::SlintResult::RepositoryExists, &path.to_string_lossy().as_ref());
-            return;
+            return None;
         }
         match create_repository_store(path) {
             Ok(store) => match self.storage.save_repository(&store) {
@@ -335,10 +339,18 @@ impl WorkerImpl {
                     let repository_id = self.repositories.add_repository(store);
 
                     self.ui_updater.new_repository(repository_id.as_i32(), ui_repository);
+
+                    Some(repository_id)
                 }
-                Err(e) => self.ui_updater.report_error(ui::SlintResult::StoreFailed, &e.to_string()),
+                Err(e) => {
+                    self.ui_updater.report_error(ui::SlintResult::StoreFailed, &e.to_string());
+                    None
+                }
             },
-            Err(e) => self.report_review_helper_error(&e),
+            Err(e) => {
+                self.report_review_helper_error(&e);
+                None
+            }
         }
     }
     fn delete_repository(&mut self, repository_id: RepositoryId) {
