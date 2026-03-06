c4f674a825115f18febff0e3e156deb89fa3d2c3
Christian von Wascinski <harryherold@googlemail.com>
2026-03-06 15:35:07 +0100

    feature(NotesView): Add and improve NotesView.

--- src/controller/file_picker_controller.rs
+++ src/controller/file_picker_controller.rs
@@ -1,11 +1,11 @@
 use std::{cell::RefCell, rc::Rc};
 
 use crate::{
-    model::ProxyModels,
+    model::{IdModel, ProxyModels, model_utils},
     repositories::{RepositoryId, ReviewId},
     ui,
 };
-use slint::{ComponentHandle, ModelRc, SharedString};
+use slint::{ComponentHandle, Model, ModelRc, SharedString};
 
 pub fn setup_file_picker(app_window: &ui::AppWindow, proxy_models: Rc<RefCell<ProxyModels>>) {
     app_window.global::<ui::SlintFilePickerAdapter>().on_file_model({
@@ -41,6 +41,15 @@ pub fn setup_file_picker(app_window: &ui::AppWindow, proxy_models: Rc<RefCell<Pr
             };
             let files_proxy_model = review_proxy_models.files_proxy_model();
             files_proxy_model.set_filter_pattern(pattern);
+        }
+    });
+    app_window.global::<ui::SlintFilePickerAdapter>().on_contains_model_context({
+        let app_window_weak = app_window.as_weak();
+        move |ids, context| -> bool {
+            let app_window = app_window_weak.unwrap();
+            let file_diff_model = model_utils::get_file_diff_model(&app_window, ids.repository_id as usize, ids.review_id as usize);
+            let file_diff_model = file_diff_model.as_any().downcast_ref::<IdModel<ui::SlintFileDiff>>().unwrap();
+            file_diff_model.iter().find(|file_diff| file_diff.file_path == context).is_some()
         }
     });
 }
